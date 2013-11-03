a0d6dab671b1eeceb3364174055cf0644cf95f7e
Mickey <sanchez.mickey@gmail.com>
2013-11-03 18:57:33 -0500

    working crate

---
+++ js/constants.js
@@ -0,0 +1,11 @@
+// set the scene size
+WIDTH = window.innerWidth;
+HEIGHT = window.innerHeight;
+	
+// set some camera attributes
+VIEW_ANGLE = 45;
+ASPECT = WIDTH / HEIGHT;
+NEAR = 0.1;
+FAR = 10000;
+
+CLOCK = new THREE.Clock();
---
+++ js/inhabitants.js
@@ -0,0 +1,15 @@
+// create the sphere's material
+var sphereMaterial = new THREE.MeshLambertMaterial({color: 0xCC0000});
+
+// create cube material
+var coolMaterial = new THREE.MeshBasicMaterial({map: THREE.ImageUtils.loadTexture('crate.jpg')});
+ 
+// set up the sphere vars
+var radius = 50, segments = 16, rings = 16;
+
+var sphere = new THREE.Mesh(new THREE.SphereGeometry(100, radius, segments, rings), sphereMaterial);
+sphere.position.x = -300;
+
+var cube = new THREE.Mesh(new THREE.CubeGeometry(100, 100, 100), coolMaterial);
+cube.position.x = 300;
+cube.rotation.y = 45;
